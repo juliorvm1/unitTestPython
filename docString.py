9c23e2cc1300bd6f43684d194912c48c44b0d167
juliorvm1 <juliorvm1@gmail.com>
2022-06-11 08:15:25 -0500

    separación de los casos de prueba en otro archivo diferente al del código

--- docString.py
+++ docString.py
@@ -8,13 +8,15 @@
 
 class user:
     """Permite representar un usuario este es un Docstring de una clase"""
-    def __init__(self,username,password)->None:
+    def __init__ (self, username: str, password: str) -> None:
         """Permite instanciar un objeto de tipo user en la primera liena de la definicion del metodo constructor 
 
         Args:
             username (str): el username del usuario
             password (str): el passwor del usuario
         """
+        self.username=username
+        self.password=password
 
 def palindromo(sentence: str)->bool:
     """Permite conocer si un string es palindromo.
@@ -24,18 +26,7 @@ def palindromo(sentence: str)->bool:
 
     Returns: 
         bool: True o False
-
-    Examples:
-
-    >>> palindromo('Anita lava la tina')
-    True
-
-    >>> palindromo('CodigoFacilito')
-    False
-
-    >>> sentence='oso'
-    >>> palindromo(sentence)
-    True
+  
     """
     sentence=sentence.lower().replace(' ','')
     return sentence==sentence[::-1]
